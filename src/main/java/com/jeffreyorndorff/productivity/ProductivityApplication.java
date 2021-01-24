69ea1500895ef4ed30c80d264691c3a399bd2e76
jeffreyo3 <jeffrey.orndorff@gmail.com>
2021-01-24 13:05:46 -0800

    Init H2 config

--- src/main/java/com/jeffreyorndorff/productivity/ProductivityApplication.java
+++ src/main/java/com/jeffreyorndorff/productivity/ProductivityApplication.java
@@ -2,7 +2,9 @@ package com.jeffreyorndorff.productivity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProductivityApplication {
 
---
+++ src/main/java/com/jeffreyorndorff/productivity/config/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains all the classes related to configurations.
+ * <p>
+ * Also see:<br>
+ * application.properties.
+ *
+ * @author Jeffrey Orndorff (jeffrey.orndorff@gmail.com) unless otherwise noted.
+ */
+package com.jeffreyorndorff.productivity.config;
